465b670a58e700880549b7f3f3473375e878a6a8
Aizhan-15 <aizhanurman@gmail.com>
2022-10-23 18:08:03 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,6 +3,9 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
         for (int i = 1; i <= sideLength; i++) {
+            if (i == 3) {
+                System.out.print('88888');
+            }
             System.out.println('8');
         }
     }
