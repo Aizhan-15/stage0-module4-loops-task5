e09279712222df7cc61e61657e69b1d2eab72c34
Aizhan-15 <aizhanurman@gmail.com>
2022-10-23 18:18:49 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -4,9 +4,9 @@ public class Square {
     public void printSquareFrom8s(int sideLength) {
         for (int x = 0; x < sideLength; x++) {
             for (int y = 0; y < sideLength; y++) {
-                System.out.println('88');
+                Square square = new Square();
             }
-            System.out.println('88');
+            System.out.println('8');
         }
     }
 }
